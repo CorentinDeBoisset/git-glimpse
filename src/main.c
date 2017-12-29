12aaa5878ef55e7a1f6c410a865c8cfc4a0b1d50
Corentin de Boisset <corentin.deboisset@gmail.com>
2017-12-29 22:34:34 +0100

    Add core functionnality

--- src/main.c
+++ src/main.c
@@ -4,21 +4,19 @@
 #include <string.h>
 #include<git2.h>
 
-struct arguments_t {
-    int verbose;
-    char *ahead_sigil;
-    char *behind_sigil;
-    char *staged_sigil;
-    char *conflicts_sigil;
-    char *unstaged_sigil;
-    char *untracked_sigil;
-    char *stashed_sigil;
-    char *clean_sigil;
-} arguments;
+struct sigils_t {
+    char *ahead;
+    char *behind;
+    char *staged;
+    char *conflicts;
+    char *unstaged;
+    char *untracked;
+    char *stashed;
+    char *clean;
+} sigils;
 
 static const char *opt_string = ":vha:b:s:c:u:U:S:C:";
 static const struct option long_opts[] = {
-    { "verbose", no_argument, NULL, 'v' },
     { "help", no_argument, NULL, 'h' },
     { "ahead-sigil", required_argument, NULL, 'a' },
     { "behind-sigil", required_argument, NULL, 'b' },
@@ -36,7 +34,6 @@ void display_usage(void)
     puts(
         "usage: fast-git-prompt [<options>]\n"
     );
-    puts("    -v, --verbose                be verbose");
     puts("    -h, --help                   show this help");
     puts("    -a, --ahead-sigil=SIG        overwrite the ahead sigil");
     puts("    -b, --behind-sigil=SIG       overwrite the behind sigil");
@@ -52,40 +49,36 @@ void display_usage(void)
 void parse_arguments(int argc, char **argv)
 {
     int opt = 0;
-    arguments.verbose = 0;
 
     while (-1 != (opt = getopt_long(argc, argv, opt_string, long_opts, NULL)))
     {
         switch(opt) {
-            case 'v':
-                arguments.verbose++;
-                break;
             case 'h':
                 display_usage();
                 break;
             case 'a':
-                arguments.ahead_sigil = optarg;
+                sigils.ahead = optarg;
                 break;
             case 'b':
-                arguments.behind_sigil = optarg;
+                sigils.behind = optarg;
                 break;
             case 's':
-                arguments.staged_sigil = optarg;
+                sigils.staged = optarg;
                 break;
             case 'c':
-                arguments.conflicts_sigil = optarg;
+                sigils.conflicts = optarg;
                 break;
             case 'u':
-                arguments.unstaged_sigil = optarg;
+                sigils.unstaged = optarg;
                 break;
             case 'U':
-                arguments.untracked_sigil = optarg;
+                sigils.untracked = optarg;
                 break;
             case 'S':
-                arguments.stashed_sigil = optarg;
+                sigils.stashed = optarg;
                 break;
             case 'C':
-                arguments.clean_sigil = optarg;
+                sigils.clean = optarg;
                 break;
             case ':':
                 fprintf(stderr, "option `-%c' requires an argument\n\n", optopt);
@@ -100,42 +93,118 @@ void parse_arguments(int argc, char **argv)
         }
     }
 
-    if (NULL == arguments.ahead_sigil)
-        arguments.ahead_sigil = "↑";
-    if (NULL == arguments.behind_sigil)
-        arguments.behind_sigil = "↓";
-    if (NULL == arguments.staged_sigil)
-        arguments.staged_sigil = "●";
-    if (NULL == arguments.conflicts_sigil)
-        arguments.conflicts_sigil = "✖";
-    if (NULL == arguments.unstaged_sigil)
-        arguments.unstaged_sigil = "✚";
-    if (NULL == arguments.untracked_sigil)
-        arguments.untracked_sigil = "…";
-    if (NULL == arguments.stashed_sigil)
-        arguments.stashed_sigil = "⚑";
-    if (NULL == arguments.clean_sigil)
-        arguments.clean_sigil = "✔";
+    if (NULL == sigils.ahead)
+        sigils.ahead = "↑";
+    if (NULL == sigils.behind)
+        sigils.behind = "↓";
+    if (NULL == sigils.staged)
+        sigils.staged = "●";
+    if (NULL == sigils.conflicts)
+        sigils.conflicts = "✖";
+    if (NULL == sigils.unstaged)
+        sigils.unstaged = "✚";
+    if (NULL == sigils.untracked)
+        sigils.untracked = "…";
+    if (NULL == sigils.stashed)
+        sigils.stashed = "⚑";
+    if (NULL == sigils.clean)
+        sigils.clean = "✔";
+}
+
+int stash_cb(size_t index, const char *message, const int *stash_id, int *payload)
+{
+    (void)index; (void)message; (void)stash_id;
+    (*payload)++;
+    return 0;
+}
+
+void get_file_status(git_repository *repo) {
+    git_status_options status_opts;
+    git_status_list *status_list;
+    size_t i, status_list_size;
+    const git_status_entry *status_entry;
+    int staged = 0;
+    int unstaged = 0;
+    int untracked = 0;
+
+    git_status_init_options(&status_opts, GIT_STATUS_OPTIONS_VERSION);
+    status_opts.flags |= GIT_STATUS_OPT_INCLUDE_UNTRACKED;
+    git_status_list_new(&status_list, repo, &status_opts);
+    status_list_size = git_status_list_entrycount(status_list);
+
+    for (i = 0; i < status_list_size; i++) {
+        status_entry = git_status_byindex(status_list, i);
+
+        /* if there are no other flags, the file is pristine */
+        if (status_entry->status == GIT_STATUS_CURRENT) {
+            continue;
+        } else if (status_entry->status == GIT_STATUS_WT_NEW) {
+            untracked++;
+        } else {
+            if (status_entry->status & (GIT_STATUS_INDEX_NEW | GIT_STATUS_INDEX_MODIFIED | GIT_STATUS_INDEX_DELETED | GIT_STATUS_INDEX_RENAMED | GIT_STATUS_INDEX_TYPECHANGE))
+                staged++;
+            if (status_entry->status & (GIT_STATUS_WT_DELETED | GIT_STATUS_WT_MODIFIED | GIT_STATUS_WT_RENAMED | GIT_STATUS_WT_TYPECHANGE))
+                unstaged++;
+        }
+    }
+    git_status_list_free(status_list);
+
+    if (staged || unstaged || untracked) {
+        printf(
+            "%s%s%s",
+            staged ? sigils.staged : "",
+            unstaged ? sigils.unstaged : "",
+            untracked ? sigils.untracked : ""
+        );
+    } else {
+        printf("%s", sigils.clean);
+    }
+}
+
+void get_commit_status(git_repository *repo) {
+    size_t ahead, behind;
+    git_reference *head, *upstream;
+
+    if (git_repository_head(&head, repo))
+        return;
+
+    if (git_branch_upstream(&upstream, head))
+        return;
+
+    git_graph_ahead_behind(&ahead, &behind, repo, git_reference_target(head), git_reference_target(upstream));
+
+    if (ahead || behind) {
+        printf("%s%s", ahead ? sigils.ahead : "", behind ? sigils.behind : "");
+    }
+}
+
+void get_stash_status(git_repository *repo) {
+    int stash_count = 0;
+    git_stash_foreach(repo, (git_stash_cb)stash_cb, &stash_count);
+    printf("%s", stash_count ? sigils.stashed : "");
 }
 
 int main(int argc, char **argv)
 {
+    char cwd[BUFSIZ];
+    git_repository *repo = NULL;
+
     parse_arguments(argc, argv);
+    getcwd(cwd, BUFSIZ);
 
-    printf(
-        "%s %s %s %s %s %s %s %s\n",
-        arguments.ahead_sigil,
-        arguments.behind_sigil,
-        arguments.staged_sigil,
-        arguments.conflicts_sigil,
-        arguments.unstaged_sigil,
-        arguments.untracked_sigil,
-        arguments.stashed_sigil,
-        arguments.clean_sigil
-    );
-    /* git_libgit2_init(); */
+    git_libgit2_init();
 
-    /* git_libgit2_shutdown(); */
+    if(0 == git_repository_open(&repo, cwd)) {
+        printf("(git:");
+        get_file_status(repo);
+        get_commit_status(repo);
+        get_stash_status(repo);
+        printf(")\n");
+
+        git_repository_free(repo);
+    }
+
+    git_libgit2_shutdown();
 
     return 0;
 }
